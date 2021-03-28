4acd568eb950d961d34f912484fba09d242112cc
LebogangBantsijang <l.bantsijangthefirst@gmail.com>
2021-03-28 17:29:53 +0200

    initial commit

--- app/src/main/java/com/lebogang/filemanager/MainActivity2.java
+++ app/src/main/java/com/lebogang/filemanager/MainActivity2.java
@@ -29,36 +29,11 @@ import com.lebogang.filemanager.managers.MediaManager;
 import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
-    private MediaManager mediaManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        check();
-        mediaManager = new MediaManager(getApplicationContext());
-        test();
     }
 
-    private void check(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 232);
-        }
-    }
-
-    private void test(){
-        List songs = mediaManager.getAudio();
-        List albums = mediaManager.getAlbums();
-        List artist = mediaManager.getArtists();
-        List<Genre> genres = mediaManager.getGenre();
-        Log.e("Songs", Integer.toString(songs.size()));
-        Log.e("Albums", Integer.toString(albums.size()));
-        Log.e("Artist", Integer.toString(artist.size()));
-        Log.e("Genres", Integer.toString(genres.size()));
-        Genre genre = genres.get(0);
-        String[] audioIds = mediaManager.getGenreAudioIds(genre.getId());
-        Log.e("Genres-Audio", Integer.toString(audioIds.length));
-        List audio = mediaManager.getAudio(audioIds);
-        Log.e(genre.getTitle(), Integer.toString(audio.size()));
-    }
 }
